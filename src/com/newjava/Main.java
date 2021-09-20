635a170ed10fdd521972c26b7e0faf06ea37a1de
Udhayakumar <udhayaarjvit1060>
2021-09-20 18:02:38 +0530

    add of 2 nos

---
+++ src/com/newjava/Main.java
@@ -0,0 +1,16 @@
+package com.newjava;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+      int a, b, c;
+        Scanner input= new Scanner(System.in);
+        c=input.nextInt();
+        b=input.nextInt();
+        a=b+c;
+        System.out.println("sum of"+a);
+
+    }
+}
